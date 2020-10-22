73e147ea4675390ebe319205538cf27863860abc
Óscar San José <osanjose@tuenti.com>
2020-10-22 22:17:33 +0200

    React working

--- escuelang/frontend/webpack.config.js
+++ escuelang/frontend/webpack.config.js
@@ -1,12 +1,34 @@
+const path = require('path');
+
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, './static/frontend'),
+    filename: 'main.js',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader", options: {babelrc: true}
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // [style-loader](/loaders/style-loader)
+          { loader: 'style-loader' },
+          // [css-loader](/loaders/css-loader)
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          // [sass-loader](/loaders/sass-loader)
+          { loader: 'sass-loader' }
+        ]
       }
     ]
   }
