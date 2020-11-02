4983484577b654106f0f5355809a641c0362eeac
Óscar San José Alonso <osanjose@tuenti.com>
2020-11-02 19:24:12 +0100

    Some play around components

--- escuelang/frontend/src/components/App.js
+++ escuelang/frontend/src/components/App.js
@@ -35,16 +35,17 @@ class App extends Component {
     return (
 <>
   <h1> Escuela de Fútbol </h1>
-  <h2> Temporada </h2>
-  <ul>
+  <h2> Temporada actual</h2>
+  <table>
+    <thead>Alumno</thead>
         {this.state.data.map(register => {
           return (
-            <li key={register.id}>
+            <tr key={register.id}>
               {register.child.name} {register.child.surname}
-            </li>
+            </tr>
           );
         })}
-      </ul>
+      </table>
 </>
     );
   }
