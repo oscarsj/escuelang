7d6211eeaf4119257ffae0c32f4bcd59dabe6e82
Oscar San Jose <oscar.sanjose@gmail.com>
2020-11-07 01:08:27 +0000

    urls.py add login urls

--- escuelang/escuelang/urls.py
+++ escuelang/escuelang/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('children.urls')),
     path('', include('frontend.urls')),
 ]
