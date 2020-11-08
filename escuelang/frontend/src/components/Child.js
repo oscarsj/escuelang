029ac72c77dcc88125e3e51da11830c7b14d2eca
Óscar San José <osanjose@tuenti.com>
2020-11-08 17:55:42 +0100

    Remove bootstrap accordeon, not helping

--- escuelang/frontend/src/components/Child.js
+++ escuelang/frontend/src/components/Child.js
@@ -5,31 +5,23 @@ import ChildDetails from './ChildDetails';
 const Child = ({child}) => {
     return (<>
 <tr>
-<td><Accordion.Toggle key={child.id} as={Card.Header} eventKey={child.id}>
-{child.name}
-</Accordion.Toggle>
+<td>
+    {child.name}
 </td>
 <td>
-<Accordion.Toggle key={child.id} as={Card.Header} eventKey={child.id}>
-{child.surname}
-</Accordion.Toggle>
+    {child.surname}
 </td>
-<td><Accordion.Toggle key={child.id} as={Card.Header} eventKey={child.id}>
+<td>
     {child.birthdate}
-</Accordion.Toggle>
 </td>
-<td><Accordion.Toggle key={child.id} as={Card.Header} eventKey={child.id}>
-    {child.address}
-</Accordion.Toggle>
+<td>{child.address}
 </td>
 </tr>
 <tr>
-<Accordion.Collapse colSpan="12" eventKey={child.id}>
 <td colSpan="12">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti esse modi cum iusto veritatis odio ullam? Voluptatibus, laboriosam delectus dolor repudiandae, doloremque voluptatem eum maiores sint, facere facilis id explicabo!
     <ChildDetails child={child}/>
 </td>
-</Accordion.Collapse>
 </tr>
 </>
 )
