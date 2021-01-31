aa6709d54b71d834252d3173cbd566bb9a2cef8f
Óscar San José <osanjose@tuenti.com>
2021-01-31 11:03:51 +0100

    Good class separation in register/editableregister

---
+++ escuelang/frontend/src/components/EditableRegister.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
+import InputChild from './InputChild';
+import InputRegister from './InputRegister';
+import EditSaveCancelButtons from './EditSaveCancelButtons';
+import trans from '../translations';
+import store from '../store';
+
+
+const EditableRegister = ({register, onRegisterUpdated, onRegisterDeleted, error, errors}) => {
+    const [showDelete, setShowDelete] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [newRegister, setNewRegisterPrivate] = useState(register);
+
+    const lang = store.useSettingsStore((state) => state.language);  
+        
+    const setNewChild = (newChild) => {
+        setNewRegisterPrivate({...newRegister,
+        child: newChild});
+    }
+    const setNewRegister = (register) => {
+        setNewRegisterPrivate({...register, child: newRegister.child});
+    }
+
+    const handleSubmit = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        setEditMode(false);
+        onRegisterUpdated(newRegister);
+    }
+
+    const handleCancelEdit = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        setNewChild(register.child);
+        setNewRegister(register);
+        console.log("Resetting original register ", register);
+        setEditMode(false);
+      }
+      
+      const cancelDelete = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        setShowDelete(false);
+    }
+
+    const handleDelete = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        setShowDelete(false);
+        setEditMode(false);
+        onRegisterDeleted(newRegister);
+    }
+    
+    return (<>
+    <Modal show={showDelete} onHide={cancelDelete}>
+        <Modal.Header closeButton>
+          <Modal.Title>{trans.seasonTranslations[lang].confirmDeleteRegisterTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{trans.seasonTranslations[lang].confirmDeleteRegister}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelDelete}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={handleDelete}>
+            Borrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        <Form onSubmit={handleSubmit}>
+        {(error && <Alert variant="danger">{error}</Alert>)}
+        <InputChild 
+            key={newRegister.child.id}
+            child={newRegister.child} 
+            onChildUpdated={setNewChild}
+            readOnly={!editMode}
+            errors={errors}/>
+        <InputRegister 
+            key={newRegister.id}
+            register={newRegister} 
+            onRegisterUpdated={setNewRegister}
+            readOnly={!editMode}
+            errors={errors}/>
+
+        <EditSaveCancelButtons 
+          editMode={editMode}
+          onSetEditMode={setEditMode} 
+          onDelete={(event) => setShowDelete(true)}
+          onCancel={handleCancelEdit}/>
+    </Form>
+    </>
+    )
+}
+
+export default EditableRegister;
--- escuelang/frontend/src/components/Register.js
+++ escuelang/frontend/src/components/Register.js
@@ -1,31 +1,16 @@
 import React, { useState } from 'react';
-import { Form, Button, Modal } from 'react-bootstrap';
-import InputChild from './InputChild';
-import InputRegister from './InputRegister';
-import EditSaveCancelButtons from './EditSaveCancelButtons';
 import store from '../store';
-import trans from '../translations';
 import childrenApi from '../client/children';
 import seasonsApi from '../client/seasons';
+import EditableRegister from './EditableRegister';
 
 const Register = ({register, visibleFields}) => {
-  const [showDelete, setShowDelete] = useState(false);
   const [rolledOut, setRolledOut] = useState(false);
-  const [editMode, setEditMode] = useState(false);
-  const [newRegister, setNewRegisterPrivate] = useState(register);
-  const [error, setError] = useState();
-  const [errors, setErrors] = useState({});
-  const lang = store.useSettingsStore((state) => state.language);  
   const storeReplaceRegister = store.useRegistersStore(state => state.replaceRegister)
   const storeDeleteRegister = store.useRegistersStore(state => state.deleteRegister);
-  
-  const setNewChild = (newChild) => {
-      setNewRegisterPrivate({...newRegister,
-          child: newChild});
-  }
-  const setNewRegister = (register) => {
-    setNewRegisterPrivate({...register, child: newRegister.child});
-  }
+  const [error, setError] = useState();
+  const [errors, setErrors] = useState({});
+
   const handleError = (err) => {
     if (err.response) {
         console.log('Error in update register', err.response);
@@ -37,13 +22,10 @@ const Register = ({register, visibleFields}) => {
         // anything else
     }
   }
-  const onRegisterSaved = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
+  const onRegisterSaved = (newRegister) => {
     childrenApi
       .update(newRegister.child.id, newRegister.child)
       .then((resultChild) => {
-        setNewChild(resultChild);
         const tmpRegister = {
             ...newRegister,
             child: resultChild.id};
@@ -51,87 +33,40 @@ const Register = ({register, visibleFields}) => {
         .updateRegister(tmpRegister.id, tmpRegister)
         .then((resultRegister) => {
             setRolledOut(false);
-            setEditMode(false);
             const tmpRegister = {
               ...resultRegister,
               child: resultChild}
             storeReplaceRegister(tmpRegister);
-            setNewRegister(tmpRegister);
           })
         .catch(handleError)
     })
   }
 
-  const onRegisterDeleted = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
+  const onRegisterDeleted = (newRegister) => {
       console.log("deleting register ", newRegister.id);
       seasonsApi
         .deleteRegister(newRegister.id)
         .then(() => {
           console.log("Register page deleted register ", newRegister.id);
           setRolledOut(false);
-          setEditMode(false);
           storeDeleteRegister(newRegister.id);
         })
         .catch(handleError)
   }
-  const handleCancelEdit = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    setNewChild(register.child);
-    setNewRegister(register);
-    console.log("Resetting original register ", register);
-    setEditMode(false);
-  }
-  const cancelDelete = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    setShowDelete(false);
-}
   return (<>
-    <Modal show={showDelete} onHide={cancelDelete}>
-        <Modal.Header closeButton>
-          <Modal.Title>{trans.seasonTranslations[lang].confirmDeleteRegisterTitle}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{trans.seasonTranslations[lang].confirmDeleteRegister}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={cancelDelete}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={onRegisterDeleted}>
-            Borrar
-          </Button>
-        </Modal.Footer>
-      </Modal>
 <tr onClick={() => setRolledOut(!rolledOut)}>
-{visibleFields.child.map((field) => <td key={`td${newRegister.child[field]}`}>{newRegister.child[field]}</td>)}
-{visibleFields.register.map((field) => <td key={`td${newRegister[field]}`}>{field=='competition'? (newRegister[field]? 'Sí':'No'):newRegister[field]}</td>)}
+{visibleFields.child.map((field) => <td key={`td${register.child[field]}`}>{register.child[field]}</td>)}
+{visibleFields.register.map((field) => <td key={`td${register[field]}`}>{field=='competition'? (register[field]? 'Sí':'No'):register[field]}</td>)}
 </tr>
 {(rolledOut && (<tr>
-    <td key={`tdUnrolled${newRegister.child.id}`} colSpan="12">
+    <td key={`tdUnrolled${register.id}`} colSpan="12">
     <div className="border border-primary rounded mb-0" style={{ padding: "10px", marginTop: "10px", marginBottom: "10px"}}>
-    <Form onSubmit={onRegisterSaved}>
-        {(error && <Alert variant="danger">{error}</Alert>)}
-        <InputChild 
-            key={newRegister.child.id}
-            child={newRegister.child} 
-            onChildUpdated={setNewChild}
-            readOnly={!editMode}
-            errors={errors}/>
-        <InputRegister 
-            key={newRegister.id}
-            register={newRegister} 
-            onRegisterUpdated={setNewRegister}
-            readOnly={!editMode}
-            errors={errors}/>
-
-        <EditSaveCancelButtons 
-          editMode={editMode}
-          onSetEditMode={setEditMode} 
-          onDelete={(event) => setShowDelete(true)}
-          onCancel={handleCancelEdit}/>
-    </Form>
+    <EditableRegister 
+      register={register}
+      onRegisterUpdated={onRegisterSaved}
+      onRegisterDeleted={onRegisterDeleted}
+      error={error}
+      errors={errors}/>
     </div>
     </td></tr>))}   
 </> 
